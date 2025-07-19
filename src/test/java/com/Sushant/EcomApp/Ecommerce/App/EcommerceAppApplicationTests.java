adbbf48bd80dc4078e074d91989d223a2d0e1da3
sushantsuman <sushantcvrcebtech17@gmail.com>
2025-07-19 12:47:06 +0530

    first commit

---
+++ src/test/java/com/Sushant/EcomApp/Ecommerce/App/EcommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sushant.EcomApp.Ecommerce.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
